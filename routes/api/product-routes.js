ef25e1dd0d4281b36664298557f1366e84458e71
NZerr <nathan.zerr@gmail.com>
2021-06-23 23:21:19 -0400

    added demo video

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -82,8 +82,6 @@ router.put('/:id', (req, res) => {
     .then((productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      console.log(productTagIds);
-      console.log(req.body.tagIds);
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
